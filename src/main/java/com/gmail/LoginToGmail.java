c0b37af195162ed195c03debdacf2d8657858314
Goralive <nosovskyigor@gmail.com>
2018-05-09 23:05:07 +0300

    Gmail test account

---
+++ src/main/java/com/gmail/LoginToGmail.java
@@ -0,0 +1,58 @@
+package com.gmail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class LoginToGmail {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    public LoginToGmail(WebDriverWait wait) {
+        this.wait = wait;
+    }
+
+    public LoginToGmail(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By loginEmail = By.xpath(".//input[@id='identifierId']");
+    private By loginPassword = By.xpath(".//div[@id='password']//input");
+    private By nextButtonEmail = By.xpath(".//div[@id='identifierNext']");
+    private By nextButtonPassword = By.xpath(".//div[@id='passwordNext']/content");
+    private By getTitleEmail = By.xpath("//title[text()[contains(.,'Jackson')]]");
+
+
+    public LoginToGmail typeEmail(String email) {
+
+        driver.findElement(loginEmail).clear();
+        driver.findElement(loginEmail).sendKeys(email);
+        return this;
+    }
+
+    public LoginToGmail typePassword(String password) {
+            if (driver.findElement(loginPassword).isEnabled()) {
+                driver.findElement(loginPassword).clear();
+                driver.findElement(loginPassword).sendKeys(password);
+            } else {
+                wait.until(ExpectedConditions.elementToBeClickable(loginPassword));
+            }
+        return this;
+    }
+
+    public LoginToGmail emailPage(String email, String password) {
+        this.typeEmail(email);
+        driver.findElement(nextButtonEmail).click();
+        this.typePassword(password);
+        driver.findElement(nextButtonPassword).click();
+        return new LoginToGmail(driver);
+    }
+
+}
